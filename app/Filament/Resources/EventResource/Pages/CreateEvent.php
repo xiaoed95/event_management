f723f278193ebd29838678c6d93e54ccab2ad6f2
faidhi <muhammadfaidhi@yahoo.com>
2023-01-11 16:59:20 +0800

    first commit

--- app/Filament/Resources/EventResource/Pages/CreateEvent.php
+++ app/Filament/Resources/EventResource/Pages/CreateEvent.php
@@ -5,8 +5,22 @@ namespace App\Filament\Resources\EventResource\Pages;
 use App\Filament\Resources\EventResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use App\Models\Event;
 
 class CreateEvent extends CreateRecord
 {
     protected static string $resource = EventResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {   
+        $user = auth()->user();
+        if  ($user->isNotAdmin() )
+        {
+            $data['user_id'] = auth()->id();
+            $data['Status'] = Event::PENDING;
+        }
+     
+
+        return $data;
+    }
 }
